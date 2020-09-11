6480475f3ff25756e049e38b4d87dcdd8e1de5b0
root <sembijpost@gmail.com>
2020-09-11 19:50:28 +0200

    klaar

--- app/src/main/java/com/sem/lvl1/MainActivity.kt
+++ app/src/main/java/com/sem/lvl1/MainActivity.kt
@@ -2,11 +2,31 @@ package com.sem.lvl1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.sem.lvl1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
+        setContentView(binding.root)
+    }
+
+    private fun checkAnswer() {
+        val answer = binding.etAnswer.text.toString()
+
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(this, getString(R.string.correct),
+            Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect),
+            Toast.LENGTH_LONG).show()
+        }
     }
 }
